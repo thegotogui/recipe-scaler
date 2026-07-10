2d1e5658787c58a187e08d5409e65285b34de855
Your Name <your-email@example.com>
2026-07-10 22:57:04 +0200

    Tweaks galore and now the new espresso machine filter monitor!

--- js/scalerRecipes.js
+++ js/scalerRecipes.js
@@ -847,6 +847,36 @@ Try a few different bread flours to see which one gives you the best result.`;
     ];
   }
 
+
+
+  // * Vodka Sauce
+  else if (name.includes("Vodka")) {
+    recipe = [
+      ['Butter', 15],
+      ['Yellow Onion', 75],
+      ['Garlic', 6],
+      ['Tomato Paste', 40],
+      ['Whole Tomatoes', 129],
+      ['Cream', 76],
+      ['Vodka', 20],
+      ['Parmigiano-Reggiano', 20],
+      ['Pasta', 140]
+    ];
+    markdown = `1. In a large (3- or 4-quart) saucepan or small Dutch oven, melt butter over medium heat. Add onion, garlic, and red pepper flakes, season lightly with salt, and cook, stirring frequently, until onions are very soft but not browned, about 15 minutes; lower heat if needed to prevent browning. 
+    
+2. Add tomato paste and cook, stirring, until tomato paste is fragrant and thick, about 3 minutes. Stir in canned tomatoes with their liquid. Bring to a simmer, then cook, stirring often and crushing the whole tomatoes roughly with a spoon, until sauce has thickened slightly, about 10 minutes.
+
+    3. Add cream, and stir to incorporate. Transfer sauce to a blender, and blend until very smooth (you may be able to make an immersion blender work, but in our tests the sauce level was too low to safely avoid splattering). Wipe out pot, then return blended sauce to it. Season lightly with salt.
+    
+    4. In a medium pot of salted boiling water, cook pasta until just shy of al dente, about 3 minutes less than the package directs. About 1 minute before you transfer pasta to sauce, add vodka to tomato sauce and bring to a gentle simmer over medium heat.
+
+    5. Using a spider skimmer or slotted spoon, transfer pasta directly to sauce pot along with 1/2 cup (120ml) pasta water (alternatively, reserve 2 cups pasta water, then drain pasta in a colander, then add to sauce with 1/2 cup of the reserved water). Increase heat to high, and cook, stirring constantly, until pasta is well coated in sauce and reaches the al dente stage, about 3 minutes. If sauce thickens too much before pasta is ready, add more pasta water in 1/4 cup (60ml) increments as needed.
+
+    6. Remove from heat and stir in cheese until thoroughly incorporated into a smooth and creamy sauce. Taste for salt, and season with more if needed. If you can't detect the vodka at all, you can add a few drops more and stir it in before serving; exactly how boozy you want the sauce is a question of taste, but be careful because a heavy hand will ruin the dish. Spoon pasta and sauce onto warmed serving plates and top with additional grated cheese. Serve immediately.
+    
+    - The vodka to sauce ratio is 1:15.83`;
+  }
+
   // * Meatballs
   else if (name.includes("Meatballs")) {
     recipe = [
